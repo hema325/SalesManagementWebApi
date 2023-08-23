7e6748546c84f30cbd6df984e10e74bf92678b9f
hema325 <hema24790@gmail.com>
2023-08-23 10:02:05 +0300

    FixingItemsReport

--- Application/Items/Queries/GetItemsReport/GetItemsReportQueryResponseProfile.cs
+++ Application/Items/Queries/GetItemsReport/GetItemsReportQueryResponseProfile.cs
@@ -11,7 +11,7 @@ namespace Application.Items.Queries.GetItemsReport
                 .ForMember(r => r.BuyingPrices, o => o.MapFrom(i => i.InvoiceItems.Where(invc => invc.Invoice.Type == Domain.Enums.Invoices.Buying).Select(invc => invc.Price - invc.Price * invc.Invoice.Discount / 100)))
                 .ForMember(r => r.SellingPrices, o => o.MapFrom(i => i.InvoiceItems.Where(invc => invc.Invoice.Type == Domain.Enums.Invoices.Selling).Select(invc => invc.Price - invc.Price * invc.Invoice.Discount / 100)))
                 .ForMember(r => r.Profit, o => o.MapFrom(i => i.InvoiceItems.Where(invc => invc.Invoice.Type == Domain.Enums.Invoices.Selling).Select(invc => invc.Price - invc.Price * invc.Invoice.Discount / 100).Sum() -
-                                                              i.InvoiceItems.Where(invc => invc.Invoice.Type == Domain.Enums.Invoices.Buying).Select(invc => invc.Price - invc.Price * invc.Invoice.Discount / 100).Sum()))
+                                                              i.InvoiceItems.Where(invc => invc.Invoice.Type == Domain.Enums.Invoices.Buying || invc.Invoice.Type == Domain.Enums.Invoices.Returning).Select(invc => invc.Price - invc.Price * invc.Invoice.Discount / 100).Sum()))
                 .ForMember(r => r.Images, o => o.MapFrom(i => i.Images.Select(img => img.Path)));
         }
     }
